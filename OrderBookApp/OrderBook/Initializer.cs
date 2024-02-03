358170551362a00f010856402f3d999bbaa4ce45
forel <lazutkinma@gmail.com>
2024-02-03 18:31:54 +0100

    Add class Initializer

---
+++ OrderBookApp/OrderBook/Initializer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderBook
+{
+    internal class Initializer
+    {
+        public static string SetTicker(string symbol)
+        {
+            string output = string.Empty;
+
+            do
+            {
+                Console.Write("Enter ticker's symbol: ");
+                output = Console.ReadLine(); 
+            } while (string.IsNullOrEmpty(output) || string.IsNullOrWhiteSpace(output));
+            
+            return output;
+        }
+
+        public static decimal SetBalanceMoney(decimal balanceMoney)
+        {
+            decimal output;
+            bool isValidNumber;
+            string outputString = string.Empty;
+
+            do
+            {
+                Console.Write("Enter money balance: ");
+                outputString = Console.ReadLine();
+                isValidNumber = decimal.TryParse(outputString, out output);
+            }
+            while (isValidNumber == false || output <= 0);
+
+            return output;
+        }
+
+        public static int SetBalanceStock(int balanceStock)
+        {
+            int output;
+            bool isValidNumber;
+            string outputString = string.Empty;
+
+            do
+            {
+                Console.Write("Enter stock balance: ");
+                outputString = Console.ReadLine();
+                isValidNumber = int.TryParse(outputString, out output);
+            }
+            while (isValidNumber == false || output <= 0);
+
+            return output;
+        }
+    }
+}
---
+++ OrderBookApp/OrderBook/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace OrderBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tickerSymbol = string.Empty;
+            decimal accountMoney = 0M;
+            int accountStock = 0;
+
+            tickerSymbol = Initializer.SetTicker(tickerSymbol);
+            accountMoney = Initializer.SetBalanceMoney(accountMoney);
+            accountStock = Initializer.SetBalanceStock(accountStock);
+
+
+            Console.WriteLine($"tickerSymbol: {tickerSymbol}\n" +
+                $"accountMoney: {accountMoney}\n" +
+                $"accountStock: {accountStock}");
+            //int.TryParse(iString, out i);
+
+
+            /*
+            Перед началом игры:
+
+            -Тикер(MSFT, GOOG)
+            // - Кол - во игроков(3 000)
+            - Кол - во денег на счете
+            - Кол - во акций на счете
+
+            Ход игрока(порядок действий):
+
+            - Трейдер передает заявку биржи
+            - Биржа регистрирует заявку
+            - Биржа исполняет заявки
+            - Биржа отображает текущее состояние стакана
+            */
+
+        }
+    }
+}
